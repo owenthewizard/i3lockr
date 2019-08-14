1ccac0f20f08a641673591097f76f1b50db7b6f9
Owen Walpole <owenthewizard@hotmail.com>
2019-08-14 00:05:29 -0500

    Dead code removal

--- src/cli/validators.rs
+++ src/cli/validators.rs
@@ -28,29 +28,6 @@ pub fn has_compose(_: String) -> Result<(), String> {
     }
 }
 
-/*
-/// Returns a closure that checks if the argument is between two numbers.
-pub fn between<T>(small: T, large: T) -> impl Fn(String) -> Result<(), String>
-where
-    T: Display + FromStr + PartialOrd,
-    T::Err: ToString,
-{
-    move |s: String| {
-        let num = match s.parse::<T>() {
-            Ok(val) => val,
-            Err(e) => return Err(e.to_string()),
-        };
-        if num >= large {
-            return Err(format!("must be smaller than {}", large));
-        }
-        if num <= small {
-            return Err(format!("must be greater than {}", small));
-        }
-        Ok(())
-    }
-}
-*/
-
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -67,19 +44,4 @@ mod tests {
         assert!(greater_than_five(five).is_err());
         assert!(greater_than_five(six).is_ok());
     }
-
-    /*
-    #[test]
-    fn between_validator() {
-        let forty_one = "41".to_string();
-        let forty_two = "42".to_string();
-        let forty_three = "43".to_string();
-
-        let at_42 = between(41, 43);
-
-        assert!(at_42(forty_one).is_err());
-        assert!(at_42(forty_two).is_ok());
-        assert!(at_42(forty_three).is_err());
-    }
-    */
 }
