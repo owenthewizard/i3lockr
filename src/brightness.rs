c31e3e2f7d781f73dfd28b11f165d8a3e79b4a02
Owen Walpole <owen@walpole.dev>
2022-06-17 15:29:27 -0500

    `Use BGRA8::map_c`

--- src/brightness.rs
+++ src/brightness.rs
@@ -6,7 +6,7 @@ use imgref::ImgRefMut;
 use rayon::prelude::*;
 
 use rgb::alt::BGRA8;
-use rgb::ComponentSlice;
+use rgb::{ColorComponentMap, ComponentSlice};
 
 pub trait BrightnessAdj {
     fn brighten(&mut self, amt: NonZeroU8);
@@ -17,44 +17,32 @@ impl BrightnessAdj for ImgRefMut<'_, BGRA8> {
     #[cfg(feature = "threads")]
     fn brighten(&mut self, amt: NonZeroU8) {
         // need to access buffer manually because imgref doesn't support rayon
-        self.buf_mut().par_iter_mut().for_each(|bgra| {
-            let mut channels = bgra.as_mut_slice().iter_mut();
-            // skip alpha
-            let _ = channels.next_back();
-            channels.for_each(|c| *c = (*c).saturating_add(amt.get()));
+        self.buf_mut().par_iter_mut().for_each(|pixel| {
+            *pixel = pixel.map_c(|c| c.saturating_add(amt.get()));
         });
     }
 
     #[cfg(not(feature = "threads"))]
     fn brighten(&mut self, amt: NonZeroU8) {
         // need to access buffer manually because imgref doesn't support rayon
-        self.buf_mut().iter_mut().for_each(|bgra| {
-            let mut channels = bgra.as_mut_slice().iter_mut();
-            // skip alpha
-            let _ = channels.next_back();
-            channels.for_each(|c| *c = (*c).saturating_add(amt.get()));
+        self.buf_mut().iter_mut().for_each(|pixel| {
+            *pixel = pixel.map_c(|c| c.saturating_add(amt.get()));
         });
     }
 
     #[cfg(feature = "threads")]
     fn darken(&mut self, amt: NonZeroU8) {
         // need to access buffer manually because imgref doesn't support rayon
-        self.buf_mut().par_iter_mut().for_each(|bgra| {
-            let mut channels = bgra.as_mut_slice().iter_mut();
-            // skip alpha
-            let _ = channels.next_back();
-            channels.for_each(|c| *c = (*c).saturating_sub(amt.get()));
+        self.buf_mut().par_iter_mut().for_each(|pixel| {
+            *pixel = pixel.map_c(|c| c.saturating_sub(amt.get()));
         });
     }
 
     #[cfg(not(feature = "threads"))]
     fn darken(&mut self, amt: NonZeroU8) {
         // need to access buffer manually because imgref doesn't support rayon
-        self.buf_mut().iter_mut().for_each(|bgra| {
-            let mut channels = bgra.as_mut_slice().iter_mut();
-            // skip alpha
-            let _ = channels.next_back();
-            channels.for_each(|c| *c = (*c).saturating_sub(amt.get()));
+        self.buf_mut().iter_mut().for_each(|pixel| {
+            *pixel = pixel.map_c(|c| c.saturating_sub(amt.get()));
         });
     }
 }
