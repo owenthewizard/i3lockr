78fdb2eb28a70d00bbe9ac28be94e6be17ca277f
Owen Walpole <owen@walpole.dev>
2022-06-17 13:18:31 -0500

    Add tests to scaling algorithm

--- src/scale.rs
+++ src/scale.rs
@@ -5,14 +5,12 @@ use imgref::ImgRefMut;
 
 use itertools::iproduct;
 
-use rgb::alt::BGRA8;
-
 pub trait Scale {
     unsafe fn scale_up(&mut self, factor: NonZeroUsize);
     unsafe fn scale_down(&mut self, factor: NonZeroUsize);
 }
 
-impl Scale for ImgRefMut<'_, BGRA8> {
+impl<T: Copy> Scale for ImgRefMut<'_, T> {
     unsafe fn scale_down(&mut self, factor: NonZeroUsize) {
         let factor = factor.get();
         let (w, h) = (self.width(), self.height());
@@ -31,5 +29,37 @@ impl Scale for ImgRefMut<'_, BGRA8> {
             *self.buf_mut().get_unchecked_mut(y * w + x) =
                 *self.buf().get_unchecked(y / factor * w + x / factor);
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use rgb::RGBA8;
+
+    const RED: RGBA8 = RGBA8::new(255, 0, 0, 255);
+    const GREEN: RGBA8 = RGBA8::new(0, 255, 0, 255);
+    const BLUE: RGBA8 = RGBA8::new(0, 0, 255, 255);
+
+    #[test]
+    fn scale() {
+        let mut data = vec![
+            RED, RED, RED, RED, GREEN, GREEN, GREEN, GREEN, BLUE, BLUE, BLUE, BLUE, RED, RED, RED,
+            RED,
+        ];
+        let mut img = ImgRefMut::new(data.as_mut(), 4, 4);
+        unsafe { img.scale_down(NonZeroUsize::new(2).unwrap()) };
+        dbg!(img.buf());
+        assert_eq!(img.buf()[..2], [RED, RED]);
+        assert_eq!(img.buf()[4..6], [BLUE, BLUE]);
+        unsafe { img.scale_up(NonZeroUsize::new(2).unwrap()) };
+        assert_eq!(
+            img.buf(),
+            &[
+                RED, RED, RED, RED, RED, RED, RED, RED, BLUE, BLUE, BLUE, BLUE, BLUE, BLUE, BLUE,
+                BLUE,
+            ]
+        );
     }
 }
