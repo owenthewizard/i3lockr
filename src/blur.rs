fcd8ccf0d5a13b0baf09d8bd91708a58f6d61ec9
Owen Walpole <owen@walpole.dev>
2022-06-23 04:43:26 -0500

    Experimental rayon+simd support, requires nightly

--- src/blur.rs
+++ src/blur.rs
@@ -5,10 +5,14 @@ use imgref::ImgRefMut;
 use rgb::alt::BGRA8;
 use rgb::ComponentSlice;
 
-#[cfg(not(feature = "threads"))]
+#[cfg(not(any(feature = "threads", feature = "simd")))]
 use stackblur_iter::blur_srgb;
-#[cfg(feature = "threads")]
+#[cfg(all(feature = "threads", not(feature = "simd")))]
 use stackblur_iter::par_blur_srgb as blur_srgb;
+#[cfg(all(feature = "simd", not(feature = "threads")))]
+use stackblur_iter::simd_blur_srgb as blur_srgb;
+#[cfg(all(feature = "simd", feature = "threads"))]
+use stackblur_iter::par_simd_blur_srgb as blur_srgb;
 
 pub trait Blur {
     fn blur(&mut self, radius: NonZeroUsize);
@@ -19,6 +23,9 @@ impl Blur for ImgRefMut<'_, BGRA8> {
         let (w, h) = (self.width(), self.height());
         let buf = unsafe { self.buf_mut().as_mut_slice().align_to_mut::<u32>().1 };
         let mut img = ImgRefMut::new(buf, w, h);
+        #[cfg(not(feature = "simd"))]
         blur_srgb(&mut img, radius.get());
+        #[cfg(feature = "simd")]
+        blur_srgb::<32>(&mut img, radius.get());
     }
 }
